48ffb3003333e0f4813860202ec5b3385ce2d676
Isha <isha.ojha@gmail.com>
2015-05-22 14:21:32 -0600

    click and trash alerts

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
 $("document").ready(function() {
-	// var newP=$("<p>");
-	// newP.append=("testing");
-//	$("#btn").click(function() {
-		$( "input" )
-		  .key(function() {
-		    var value = $(this).val();
-		    	$("#btn").click(function() {
-		    		$(".list>ul").append("<li>" + "<.item>" value + "</li>");
-				});			
-			});
+			// $( "input" )
+			//   .keyup(function() {
+			//     var value = $(this).val();
+			//     $(".list>ol").append("<li><div class='item'>" + value + "</div></li>");
+			// 	});
+
+		    $("#btn").click(function() {
+		    var value = $('input').val();
+		    if ($('.sample').length) {
+		    	$('.sample').remove()
+		    }	
+
+		    var template = $('#template li');
+		    template.find('.item').text(value);
+		    
+		    $(".list>ol").append(template);
+		    $('input').val('');
+		    });
+
+		 //   $(".list>ol").append("<li><div class='item'>" + value + "</div></li>");
+
+		    $('.check>i').click(function(){
+		    alert('works');
+		    });
+
+		    $('.delete>i').click(function(){
+		    alert('works!');
+		    });
 });
 
 
+
+
+// Resources:
+
+// $(".thumbs").sortable({
+//     stop: function(event, ui) {
+//       $(this).find('.neutral').appendTo("div li:last-of-type");
+//     }
+// });
+	
+//		     $('.delete').on('click',(function(){
+//		     	$('.list>li').toggle('slow');
+//		     })
