f755aea41af42024f639f08e6dab6e44a8255632
Isha <isha.ojha@gmail.com>
2015-05-25 19:29:56 -0600

    Click/unclick added

--- js/script.js
+++ js/script.js
@@ -5,30 +5,81 @@ $("document").ready(function() {
 			//     $(".list>ol").append("<li><div class='item'>" + value + "</div></li>");
 			// 	});
 
-		    $("#btn").click(function() {
-		    var value = $('input').val();
-		    if ($('.sample').length) {
-		    	$('.sample').remove()
-		    }	
+		    $("#btn").on('click', function() {
+			    var value = $('input').val();
+			    if ($('.sample').length) {
+			    	$('.sample').remove()
+			   	}
 
-		    var template = $('#template li');
-		    template.find('.item').text(value);
-		    
-		    $(".list>ol").append(template);
-		    $('input').val('');
+			    var template = $('#template li');
+			    template.addClass('unchecked');
+			    template.find('.item').text(value);
+			    
+			    $(".list>ol").append(template);	 
+			   	$('input').val('');
+			});
+
+		    //Delete item
+		    $('.delete>i').on('click', function(){
+		    	$(this).closest('li').remove();
 		    });
 
-		 //   $(".list>ol").append("<li><div class='item'>" + value + "</div></li>");
+		    //Checked item red & at bottom of list
+					    
+		    $('.check>i').on('click', function(){
+		     	var currentItem = $(this).closest('li');
+		     	if (currentItem.hasClass('unchecked')) {
+		     		currentItem.removeClass('unchecked');
+		     		currentItem.addClass('checked');
+		     	} else {
+		     		currentItem.removeClass('checked');
+		     		currentItem.addClass('unchecked');
+		     	}
 
-		    $('.check>i').click(function(){
-		    alert('works');
-		    });
-
-		    $('.delete>i').click(function(){
-		    alert('works!');
-		    });
+		    // 	$('ol').append(currentItem);
+		    //	$(move).css('color','red');
+		    //	$('ol').append(currentItem);
+			});
 });
 
+/*		    	checked.find('.item').text(template.find('.item').val());
+		    	template.remove();
+
+		    	$('.list>ol').append(checked);
+		    	checked.css('color','red');
+		    });
+
+*/
+
+
+
+
+//
+// $(document).ready(function(){
+//    $('ul.list').find('li.special').appendTo('ul.list');
+// });
+
+
+
+
+/*    $(document).keyup(function (e){
+    	if (e.keyCode == 88)
+    	$('#btn').css();
+    	$('.ryu-ready').show();
+})
+
+		   	$('#btn').attr('disabled',true);
+			$('input').keyup(function() {
+   				if($(this).val().length ===0)
+   					$('#btn').attr('disabled', false);
+   				else
+   					$('#btn').attr('disabled',true);
+   				});
+
+*/
+
+
+//$('ul').on('click', 'li', function() { code... });
 
 
 
